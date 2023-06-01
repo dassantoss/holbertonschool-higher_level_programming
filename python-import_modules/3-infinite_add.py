cbbfd55975cf43d66e83c720c84a60a77b2aabdf
Santiago E <santy.e@gmail.com>
2023-06-01 15:40:01 -0600

    Delete idigit validation

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,5 @@ if __name__ == "__main__":
     import sys
     result = 0
     for arg in sys.argv[1:]:
-        if arg.isdigit():
-            result += int(arg)
+        result += int(arg)
     print(result)
