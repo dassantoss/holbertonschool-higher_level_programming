0025a7b662de94c822ded6a441b5f16c63926e07
Santiago E <santy.e@gmail.com>
2023-06-01 16:30:38 -0600

    Fix

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
-    for var in dir(variable_load_5):
-        if not var.startswith('__'):
-            print("{}".format(getattr(variable_load_5, var)))
+    from variable_load_5 import a
+    print(a)
