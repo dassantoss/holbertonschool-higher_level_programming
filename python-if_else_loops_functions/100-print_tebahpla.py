2d16bc84c3a9bc653d00e2628253f0dbba25941d
Santiago E <santy.e@gmail.com>
2023-06-01 09:39:44 -0600

    Fix

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for c in reversed(range(65, 91)):
-    if c % 2 != 0:
-        print(chr(c), end="")
-    else:
-        print(chr(c + 32), end="")
+for ch in reversed(range(65, 91)):
+    print("{:c}".format(ch if ch % 2 != 0 else ch + 32), end="")
+    
