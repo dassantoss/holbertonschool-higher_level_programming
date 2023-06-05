2184fbf5f162c5645436d88542549f40f4a56742
Santiago E <santy.e@gmail.com>
2023-06-05 16:54:08 -0600

    Fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return
-    elif idx != len(my_list):
+    elif idx > len(my_list):
         return
     else:
-        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
+        return my_list[idx]
