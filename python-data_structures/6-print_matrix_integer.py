1a54b7a824c0161b4bd241d1ad46af0b06441983
Santiago E <santy.e@gmail.com>
2023-06-05 19:07:44 -0600

    Test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
+    if matrix is None or len(matrix) == 0:
         return
     for row in matrix:
         for element in row:
