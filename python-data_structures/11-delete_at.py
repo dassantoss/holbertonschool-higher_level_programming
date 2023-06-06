d48d17b3790643af4814732d282df76808092348
Santiago E <santy.e@gmail.com>
2023-06-05 21:20:52 -0600

    Fix validation

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if not my_list:
         return
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return (my_list)
     else:
         del my_list[idx]
