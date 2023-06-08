eece0962a8c6268b9d54790ffee48f46877e68d7
Santiago E <santy.e@gmail.com>
2023-06-08 15:51:38 -0600

    Fix codestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
-    squared_matrix = matrix.copy()
-    squared_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
-    return list(squared_matrix)
+    s_matrix = matrix.copy()
+    s_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    return list(s_matrix)
