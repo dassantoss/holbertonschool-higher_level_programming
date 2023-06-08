9c3e5e110c2f1f87055e88bb7335bc57fc069493
Santiago E <santy.e@gmail.com>
2023-06-08 16:13:14 -0600

    Fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     s_matrix = []
     for row in matrix:
         s_matrix.append(list(map(lambda x: x**2, row)))
-    return list(s_matrix)
+    return (s_matrix)
