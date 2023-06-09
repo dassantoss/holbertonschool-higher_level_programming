8e543358fef75d5a5e81a715a22c1c8f6e076a4b
Santiago E <santy.e@gmail.com>
2023-06-09 08:57:47 -0600

    Add Technical interview preparation: Roman Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+def roman_to_int(roman_string):
+    if not roman_string:
+        return (0)
+
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    valor = 0
+    valor_anterior = 0
+    total = 0
+
+    for letra in reversed(roman_string):
+        valor = roman_values[letra]
+
+        if valor < valor_anterior:
+            total -= valor
+        else:
+            total += valor
+        valor_anterior = valor
+
+    return (total)
