e3813c74a20cded4046fdd04987bb9ae6d90b778
Santiago E <santy.e@gmail.com>
2023-06-09 21:49:11 -0600

    Fix

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+    except:
+        return (False)
+    else:
+        return (True)
