1da4c0254d3c1cd2c9d41eeaa1e2c70e792b4423
Santiago E <santy.e@gmail.com>
2023-06-09 22:14:57 -0600

    Fix codestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             div = 0
             if i < len(my_list_1) and i < len(my_list_2):
-                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                if isinstance(my_list_1[i], (int, float)) and \
+                   isinstance(my_list_2[i], (int, float)):
                     if my_list_2[i] != 0:
                         div = my_list_1[i] / my_list_2[i]
                     else:
