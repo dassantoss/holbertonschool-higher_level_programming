77ef86beb1b8c64fda81bc713ac50717d9d3d987
Santiago E <santy.e@gmail.com>
2023-06-09 18:16:44 -0600

    Upgrade: Add keys to delete validation

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dict = a_dictionary.copy()
+    new_dictionary = a_dictionary.copy()
+    keys_to_delete = []
 
-    for key, val in a_dictionary.items():
+    for key, val in new_dictionary.items():
         if val == value:
-            del new_dict[key]
-    return (new_dict)
+            keys_to_delete.append(key)
+
+    for key in keys_to_delete:
+        del new_dictionary[key]
+
+    return new_dictionary
