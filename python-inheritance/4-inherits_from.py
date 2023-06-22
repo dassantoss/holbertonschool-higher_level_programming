68ff7f19f333ebe5490a72eb5517c9f9e803122f
Santiago E <santy.e@gmail.com>
2023-06-22 14:42:22 -0600

    Fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
     Returns:
         bool: Returns True or Flase
     '''
-    return issubclass(type(obj), a_class)
+    return type(obj) != a_class and issubclass(type(obj), a_class)
