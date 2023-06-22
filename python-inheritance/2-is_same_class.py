c0bc42bec1565241ebbc764d1e82f7df60f2d980
Santiago E <santy.e@gmail.com>
2023-06-22 14:21:02 -0600

    Upgrade: Change isistance for type is a_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -12,4 +12,4 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     '''
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
