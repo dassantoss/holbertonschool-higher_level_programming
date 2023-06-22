b0608b2b636582c9dad6455a75111f2153945802
Santiago E <santy.e@gmail.com>
2023-06-22 14:29:13 -0600

    Fix styles

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -8,7 +8,6 @@ def is_kind_of_class(obj, a_class):
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
-        
     Returns:
         Returns True if the object is an instance of,
         or if the object is an instance of a class that inherited from,
