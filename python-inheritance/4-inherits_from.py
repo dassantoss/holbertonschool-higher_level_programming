b1d685631455d4674f311204b4ef225dc6937b59
Santiago E <santy.e@gmail.com>
2023-06-22 14:38:41 -0600

    Add 4. Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Function inherits_from()'''
+
+
+def inherits_from(obj, a_class):
+    '''Check if an object is an instance of a class that inherited
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to check for inheritance from.
+
+    Returns:
+        bool: Returns True or Flase
+    '''
+    return issubclass(type(obj), a_class)
