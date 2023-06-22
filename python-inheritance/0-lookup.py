04418774c33a065ab16e9f8a6840a9ff89a4a383
Santiago E <santy.e@gmail.com>
2023-06-21 18:16:46 -0600

    Upgrade: Add comments

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """This function returns the list of available attributes and methods of an object
+    
+    Args:
+        obj: The object to inspect
+        
+    Returns:
+        Returns a list object
+    """
     return dir(obj)
