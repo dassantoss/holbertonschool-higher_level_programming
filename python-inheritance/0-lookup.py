b94829fbbc33f11d0b0d604543f514a56cfb96b0
Santiago E <santy.e@gmail.com>
2023-06-22 12:42:06 -0600

    Fix comments and styles

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Function that returns list
-"""
+'''Function that returns list
+'''
 
 
 def lookup(obj):
-    """Function lookup()
-    
+    '''Function lookup()
+
     Args:
         obj: The object to inspect
-        
+
     Returns:
         Returns a list object
-    """
+    '''
     return dir(obj)
