f2c2377b9ad95f7eb66510e151fb5f9e85dc6279
Santiago E <santy.e@gmail.com>
2023-06-25 22:02:16 -0600

    Fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ import sys
 import os.path
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 if os.path.exists("add_item.json"):
