cd3a568cfbdbde4ae89fe061944e2f258e1d289b
Santiago E <santy.e@gmail.com>
2023-06-28 17:34:27 -0600

    Upgrade: Add to_json_string meth

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Define class base'''
 
+import json
+
 
 class Base:
     '''Represent a class base'''
@@ -17,3 +19,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Method to_json_string
+        Returns:
+             The JSON string representation of list_dictionaries
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
