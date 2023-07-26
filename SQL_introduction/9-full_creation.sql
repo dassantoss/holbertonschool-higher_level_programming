c53c4050dfeca291d85bd3222ec2c58263ebaf89
Santiago E <santy.e@gmail.com>
2023-07-25 20:04:53 -0600

    Add script that creates a table with multiple rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- TAsk 9
+-- Create the table second_table in the hbtn_0c_0 database.
+
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	SCORE INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
