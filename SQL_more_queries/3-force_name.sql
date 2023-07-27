746bbd690696acbc3367a9cdf8e32f981ef9ed4e
Santiago E <santy.e@gmail.com>
2023-07-27 15:04:32 -0600

    Add script that creates the table

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Script to create the table force_name in MySQL server.
+-- Query to create the table force_name.
+
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+    name VARCHAR(256) NOT NULL
+);
