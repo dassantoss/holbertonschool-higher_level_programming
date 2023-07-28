2fdec2b98f9a125284889423e08d1a6d79d49416
Santiago E <santy.e@gmail.com>
2023-07-28 07:52:49 -0600

    Add Script that creates the db and table using Foreign Key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Script that creates the database and the table.
+-- Query to create the database and table.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+		id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+		state_id INT NOT NULL,
+		name VARCHAR(256) NOT NULL,
+		FOREIGN KEY (state_id) REFERENCES states(id) 
+);
