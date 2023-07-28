a2d18e3251825ad9e0db8b1afbd6735814207279
Santiago E <santy.e@gmail.com>
2023-07-28 16:32:06 -0600

    Add  Script to list all cities using JOIN

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Script to list all cities with their corresponding state names from the database hbtn_0d_usa.
+
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+JOIN states 
+ON cities.state_id = states.id 
+ORDER BY cities.id;
