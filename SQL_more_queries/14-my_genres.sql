4f146c1405b8c4e1ee3f68e853b45808bcced401
Santiago E <santy.e@gmail.com>
2023-07-28 18:10:39 -0600

    Fix sorted

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,4 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
 WHERE tv_shows.title = 'Dexter' 
-ORDER BY tv_shows.title;
+ORDER BY tv_genres.name;
