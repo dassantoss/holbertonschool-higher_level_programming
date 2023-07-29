b55b1367094d759b89183df36870766e82d64288
Santiago E <santy.e@gmail.com>
2023-07-28 18:16:20 -0600

    Add Script to Find the titles classified as Comedy.

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Script to Find the titles of all TV shows that are classified as Comedy.
+
+SELECT tv_shows.title 
+FROM tv_shows 
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_genres.name = 'Comedy' 
+ORDER BY tv_shows.title;
