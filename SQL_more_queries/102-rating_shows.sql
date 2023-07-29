a7e672b45bab0a68b2534570f5b08b08a43466e8
Santiago E <santy.e@gmail.com>
2023-07-29 16:20:36 -0600

    Add Script to get the rating of each show.

---
+++ SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Script to get the rating of each show.
+
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
