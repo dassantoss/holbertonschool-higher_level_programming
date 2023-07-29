3f791307a1c32e67c643272e2878d33b02de43d3
Santiago E <santy.e@gmail.com>
2023-07-28 18:08:16 -0600

    Add Script to Find the genres of the TV show Dexter.

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Script to Find the genres of the TV show Dexter.
+
+SELECT tv_genres.name 
+FROM tv_genres 
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_shows.title = 'Dexter' 
+ORDER BY tv_shows.title;
