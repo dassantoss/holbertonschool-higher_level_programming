1a9a0b865c26d7f911f18bf6195a45685cf13097
Santiago E <santy.e@gmail.com>
2023-08-02 15:48:29 -0600

    Add script that takes in an argument and displays all values

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table where
+name matches the argument.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * \
+                   FROM states \
+                   WHERE name LIKE '{}' \
+                   ORDER BY id".format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
