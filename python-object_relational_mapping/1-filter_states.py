44db1ce14b3ce319ab22228995c2610e7aaa8a2c
Santiago E <santy.e@gmail.com>
2023-08-02 15:14:49 -0600

    Add script that lists all states with a name starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve states with names starting with N
+    cursor.execute("SELECT * \
+                   FROM states \
+                   WHERE name LIKE 'N%' \
+                   ORDER BY id")
+
+    # Fetch all the rows and display them
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    # Close the cursor and connection to the database
+    cursor.close()
+    db.close()
