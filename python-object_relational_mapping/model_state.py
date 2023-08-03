5d72a257173ed1b47ec8b0b036b748b6f2fea89e
Santiago E <santy.e@gmail.com>
2023-08-02 22:45:49 -0600

    Add First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that links to the MySQL table 'states'.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, autoincrement=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
