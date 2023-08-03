83004b016083c2cdaa7a260b2bffbd0637522d6e
Santiago E <santy.e@gmail.com>
2023-08-02 19:26:00 -0600

    Add Script that lists all cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=sys.argv[1], passwd=sys.argv[2],
+                        db=sys.argv[3])
+
+    cursor = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            JOIN states \
+            ON cities.state_id = states.id \
+            ORDER BY cities.id"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
