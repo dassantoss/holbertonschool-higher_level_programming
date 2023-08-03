cbf096335c34a8aa6baa93764f1243e31ea9deab
Santiago E <santy.e@gmail.com>
2023-08-03 11:45:06 -0600

    Add Script that prints the first State object using ORM

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    row = session.query(State).order_by(State.id).first()
+    if row is not None:
+        print("{}: {}".format(row.id, row.name))
+    else:
+        print("Nothing")
+
+    session.close()
