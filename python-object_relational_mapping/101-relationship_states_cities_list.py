5cf429804d96f892c8fdcf616c09c0dbf2e58f73
Santiago E <santy.e@gmail.com>
2023-08-03 19:53:07 -0600

    Fix print

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
     states = session.query(State).order_by(State.id).all()
     for state in states:
-        print(state)
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print(city)
+            print("    {}: {}".format(city.id, city.name))
 
     session.close()
