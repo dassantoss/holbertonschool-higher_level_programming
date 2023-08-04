8cae0cf9fda2ef6560996fb8808753e7aa3416cc
Santiago E <santy.e@gmail.com>
2023-08-03 18:17:02 -0600

    Add script that deletes all States with letter a using ORM

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that delete a State
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_to_delete = session.query(State).filter(State.name.like("%a%")).all()
+    states_to_delete.delete(synchronize_session=False)
+    session.commit()
+
+    session.close()
