f13eea517c1ec9dda1f18eb499878c96ae819878
Santiago E <santy.e@gmail.com>
2023-09-19 17:50:38 -0600

    Add script that updates the text of the element

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// Utiliza jQuery para seleccionar el elemento con el ID "update_header" y agrega un evento de clic
+$('#update_header').click(function() {
+	// Selecciona el elemento <header> y actualiza su texto
+	$('header').text('New Header!!!');
+  });
+  
